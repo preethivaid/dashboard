fd6383599bb1fbdbb017c2561e623f06a4b207b3
Preethi <preethivaid@gmail.com>
2019-01-12 19:20:31 -0500

    Remove the auth app- not necessary

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('auth/', include('auth.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
